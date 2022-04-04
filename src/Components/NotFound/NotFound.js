a2672ad7192b608e143a80baa46a8ce5e855411c
faisalkn-71 <csust2021@gmail.com>
2022-04-04 13:58:11 +0600

    header-done

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>The page you are looking for is not found!!!!</h3>
+            <br></br>
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default NotFound;
