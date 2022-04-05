597654a926dcdc960d188cce33ba52b938a7e009
faisalkn-71 <csust2021@gmail.com>
2022-04-05 12:34:11 +0600

    home-page-reviews-done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useReviews from '../../hooks/useReviews';
 import { Link } from 'react-router-dom';
 import Review from '../Review/Review';
 import './Home.css'
+import image from '../image/hp-spectre.jpg'
 
 const Home = () => {
     const [reviews, setReviews] = useReviews([])
+
+    // useEffect(() => {
+    //     const newReviews = reviews.slice(1, 4);
+
+    //     setReviews(newReviews);
+    // },[])
 
     // console.log(reviews.slice(1, 4))
     // const editedReview = reviews.slice(1, 4)
@@ -16,11 +23,11 @@ const Home = () => {
             <div className='front-show'>
                 <div className='home-detail'>
                     <h1>Your next Laptop </h1>
-                    <h1>Your best Laptop</h1>
+                    <h1 className='special-title'>Your best Laptop</h1>
                     <p>Nowadays, finding a best laptop it's quite impossible. For that reason we are coming with this brand new shop for providing you the best laptop in your zone. You can find here the best laptop for web-developing, gaming, coding and many more works. </p>
                     <p>So, why do you waiting for? Come here and take your needed one.</p>
                 </div>
-                <img src="../image/hp-spectre.jpg" alt="" />
+                <img className='home-img' src={image} alt="" />
 
                 
             </div>
@@ -29,7 +36,7 @@ const Home = () => {
                 <h2>Customers Reviews</h2>
                 <div  className='reviews-section'>
                 {
-                    reviews.map(review => <Review
+                    reviews.slice(1, 4).map(review => <Review
                     key = {review.id}
                     review = {review}
                     ></Review>)
